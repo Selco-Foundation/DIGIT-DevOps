c3705c1375435c060c8ac87a8a77a121740c74bc
Pasam6 <Prasad.pasam@beehyv.com>
2026-03-04 16:48:50 +0530

    upgrade eks 1.32->1.35

--- infra-as-code/terraform/sample-aws/providers.tf
+++ infra-as-code/terraform/sample-aws/providers.tf
@@ -2,6 +2,15 @@
 # Provider Configuration
 #
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "ap-south-1"
 }
